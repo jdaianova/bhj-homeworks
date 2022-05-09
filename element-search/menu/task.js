bba002ff3afd67a68abb55493366899681759380
Julia D <jdaianova@gmail.com>
2022-05-09 10:24:46 +0200

    4 commit

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -2,7 +2,7 @@ const menuLinks = document.querySelectorAll('.menu__link');
 
 for (let i = 0; i < menuLinks.length; i++) {
   menuLinks[i].onclick = function () {
-    if (menuLinks[i].nextElementSibling.className.includes('menu_sub')) {
+    if (menuLinks[i].nextElementSibling !== null) {
       menuLinks[i].nextElementSibling.classList.toggle('menu_active');
     };
     return false;
