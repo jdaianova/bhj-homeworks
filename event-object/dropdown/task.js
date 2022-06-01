7e52f93f0b1e9bed23e745486d6855eb020994db
Julia D <jdaianova@gmail.com>
2022-06-01 14:02:39 +0200

    2 commit

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,6 +1,17 @@
-document.querySelector('.dropdown__value').onclick = function (event) {
-    //console.log(event);
-    event.target.nextElementSibling.classList.toggle('dropdown__list_active');
+const mainMenu = document.querySelector('.dropdown__value');
 
+mainMenu.addEventListener ('click', () => {
 
-    };
+    mainMenu.nextElementSibling.classList.toggle('dropdown__list_active');
+
+    const submenuLinks = mainMenu.nextElementSibling.childNodes;
+
+    for (let i = 0; i < submenuLinks.length; i++) {
+        submenuLinks[i].addEventListener('click', () => {
+            mainMenu.innerText = submenuLinks[i].innerText;
+            return false;
+        });        
+    }
+
+});
+
