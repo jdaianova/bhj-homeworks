e1dc69b588b025adaa17c41ad9fb492ffaebc2df
Julia <jdaianova@gmail.com>
2022-07-23 23:19:32 +0200

    2 commit

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -1,12 +1,23 @@
 let url = 'https://netology-slow-rest.herokuapp.com/upload.php';
 const progress = document.getElementById( 'progress' );
 
-var xhr = new XMLHttpRequest();
-xhr.open("GET", url);
-xhr.send();
+  function upload(file) {
+    let xhr = new XMLHttpRequest();
 
+    xhr.upload.onprogress = function(event) {
+        progress.value = event.loaded;
+    };
+  
+    xhr.onloadend = function() {
+      if (xhr.status == 200) {
+        console.log("Успех");
+      } else {
+        console.log("Ошибка " + this.status);
+      }
+    };
+  
+    xhr.open("POST", url);
+    xhr.send(file);
+  }
 
-console.log(xhr);
-
-
-//progress.value = 0.7;
+  upload(file);
